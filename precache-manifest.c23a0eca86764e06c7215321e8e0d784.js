d32f24efffe62c99539ce233d96d6f9233115c87
Jordan2551 <jordan1158@gmail.com>
2020-03-08 00:02:48 -0500

    Updates

---
+++ precache-manifest.c23a0eca86764e06c7215321e8e0d784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8441bb000b6d7de043f4799e15b3452d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45b87f144f858f76a479",
+    "url": "/monsters-rolodex/static/css/main.eb936432.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b87f144f858f76a479",
+    "url": "/monsters-rolodex/static/js/main.4c36ff78.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
